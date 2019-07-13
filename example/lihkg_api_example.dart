ba48275bf54255d0d83d4063f05ffe626b948d84
johntyty912 <johntyty912@gmail.com>
2019-07-13 14:22:43 +0800

    example for push post

--- example/lihkg_api_example.dart
+++ example/lihkg_api_example.dart
@@ -12,12 +12,14 @@ main() async {
   Map<String, dynamic> loginBody =
           json.decode(await File('user.json').readAsString());
   loginResponse = await _client.postLogin(loginBody);
+  print(loginResponse.success == 1? "login success": "fail: ${replyResponse.errorMessage}");
 
-  String threadID = "1302846";
+  String threadID = "1304081";
   String content = "PUSH";
 
   const oneMin = const Duration(seconds:60);
   Timer.periodic(oneMin, (Timer t) async {
     replyResponse = await _client.postReply(threadID, content);
+    print(replyResponse.success == 1? "reply success": "fail: ${replyResponse.errorMessage}");
   });
 }
